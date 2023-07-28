35cd925d553b5fb49b06693fec10975d4acb3d24
Moshe Yaku <mosheyaku@gmail.com>
2023-07-28 16:55:24 +0300

    Adding Item class.

--- src/main/java/com/example/restaurantorder/RestaurantOrderController.java
+++ src/main/java/com/example/restaurantorder/RestaurantOrderController.java
@@ -1,14 +1,38 @@
 package com.example.restaurantorder;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
 
 public class RestaurantOrderController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private GridPane grid;
+
+    private ArrayList<TextField> description;
+
+    private ArrayList<TextField> type;
+
+    private ArrayList<ComboBox> quantity;
+
+    private ArrayList<TextField> price;
+
+    private ArrayList<CheckBox> select;
+
+    @FXML
+    public void orderPressed(ActionEvent actionEvent) {
+    }
+
+    public void initialize() {
+        select = new ArrayList<CheckBox>();
+        quantity = new ArrayList<ComboBox>();
+        type = new ArrayList<TextField>();
+        description = new ArrayList<TextField>();
+        price = new ArrayList<TextField>();
     }
 }
