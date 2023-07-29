8c8bc071687c918c13357574e7f34495c1686962
Moshe Yaku <mosheyaku@gmail.com>
2023-07-29 23:42:11 +0300

    Adding method that gets the details of the order.

--- src/main/java/com/example/restaurantorder/Order.java
+++ src/main/java/com/example/restaurantorder/Order.java
@@ -5,16 +5,14 @@ import java.util.ArrayList;
 
 public class Order implements Serializable {
 
+    private final int DIGIT_AFTER_POINT = 2;
+
     private ArrayList<Item> items;
     private double payment;
 
     public Order() {
         items = new ArrayList<>();
         payment = 0;
-    }
-
-    public ArrayList<Item> getItems() {
-        return items;
     }
 
     public double getPayment() {
@@ -25,5 +23,31 @@ public class Order implements Serializable {
         items.add(item);
         payment += item.getTotalPrice();
     }
+
+    public String getOrderDetails() {
+        StringBuilder orderDetails = new StringBuilder();
+        for (Item item : items) {
+            orderDetails.append(item.getDescription()).append(" - $");
+            orderDetails.append(convertPriceToString(item.getPrice())).append(" x ");
+            orderDetails.append(item.getQuantity()).append(" = $");
+            orderDetails.append(convertPriceToString(item.getTotalPrice())).append("\n");
+        }
+        orderDetails.append("Total: $").append(convertPriceToString(payment));
+        return orderDetails.toString();
+    }
+
+    private String convertPriceToString(double amount) {
+        String cash = amount + "";
+        int k, count = 0;
+        boolean gotToPoint = false;
+        for (k = 0; k < cash.length() && count != DIGIT_AFTER_POINT; k++) {
+            if (gotToPoint)
+                count++;
+            if (cash.charAt(k) == '.')
+                gotToPoint = true;
+        }
+        return cash.substring(0, k);
+    }
+
 }
 
